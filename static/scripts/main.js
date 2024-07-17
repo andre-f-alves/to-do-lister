0d366d107d21e686de692c53d2088a304b6c797f
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-07-17 18:41:53 -0300

    Sidebar adjustments

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,12 +1,12 @@
-const sidebar = document.querySelector('.sidebar')
+const sidebar = document.querySelector('.sidebar-container')
 const menuButton = document.querySelector('.menu-button')
 
 menuButton.addEventListener('click', function() {
-  const icon = this.firstElementChild
+  const icon = document.getElementById('sb-ctrl-icon')
   const url = new URL(icon.src)
   const filePath = url.pathname.split('/')
 
-  filePath[filePath.length - 1] = filePath[filePath.length - 1] === 'menu.svg' ? 'close.svg' : 'menu.svg'
+  filePath[filePath.length - 1] = filePath[filePath.length - 1] === 'open-sidebar.svg' ? 'close-sidebar.svg' : 'open-sidebar.svg'
 
   url.pathname = filePath.join('/')
   icon.src = url.toString()
