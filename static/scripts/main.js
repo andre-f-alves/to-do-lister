66d0f76f030b047f53c678a0e7e5c29d5c0e7e1b
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-07-20 18:05:36 -0300

    Functional add file button

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,8 +1,10 @@
-const sidebar = document.querySelector('.sidebar-container')
-const menuButton = document.querySelector('.menu-button')
+const menuButton = document.getElementById('menu-button')
+const newListButton = document.getElementById('new-list-button')
 
-menuButton.addEventListener('click', function() {
-  const icon = document.getElementById('sb-ctrl-icon')
+function changeSidebarVisibility() {
+  const sidebar = document.getElementById('sidebar')
+
+  const icon = document.getElementById('sidebar-control-icon')
   const url = new URL(icon.src)
   const filePath = url.pathname.split('/')
 
@@ -19,4 +21,31 @@ menuButton.addEventListener('click', function() {
   icon.src = url.toString()
 
   sidebar.classList.toggle('active')
-})
+}
+
+function addFile() {
+  const fileList = document.getElementById('file-list')
+  const template = document.getElementById('file-list-item-template')
+  const fileListItemTemplate = template.content.querySelector('.file-list-item')
+
+  if (document.getElementById('empty-message')) {
+    fileList.removeChild(document.getElementById('empty-message'))
+  }
+  
+  let fileName
+  do {
+    fileName = prompt('Digite um nome para o arquivo:').trim()
+    if (fileName === null) return
+  } while (!fileName)
+
+  const fileListItem = document.importNode(fileListItemTemplate, true)
+  const fileLink = fileListItem.firstElementChild
+
+  fileLink.textContent = fileName
+  fileLink.title = fileName
+
+  fileList.appendChild(fileListItem)
+}
+
+menuButton.addEventListener('click', function() { changeSidebarVisibility.call(this) })
+newListButton.addEventListener('click', () => addFile())
