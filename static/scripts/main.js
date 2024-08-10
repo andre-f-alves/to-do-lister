96ee73c480c710eef49d1bb760be1a9954c4a9b0
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-08-10 15:47:17 -0300

    File storage

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,51 +1,41 @@
-const menuButton = document.getElementById('menu-button')
+const sidebarButton = document.getElementById('sidebar-button')
 const newFileButton = document.getElementById('new-file-button')
 const fileNameDialog = document.getElementById('file-name-dialog')
 const fileNameInput = document.getElementById('file-name-input')
 const submitButton = document.getElementById('submit-button')
 const cancelButton = document.getElementById('cancel-button')
 
-function toggleSidebarVisibility() {
-  const sidebar = document.getElementById('sidebar')
+const fileList = document.getElementById('file-list')
+const fileListItemTemplate = document.getElementById('file-list-item-template')
+  .content.querySelector('.file-list-item')
 
-  const icon = this.firstElementChild
-  const url = new URL(icon.src)
-  const filePath = url.pathname.split('/')
-
-  if (filePath[filePath.length - 1] === 'open-sidebar.svg') {
-    filePath[filePath.length - 1] = 'close-sidebar.svg'
-    this.title = this.title.replace('Abrir', 'Fechar')
-
-  } else {
-    filePath[filePath.length - 1] = 'open-sidebar.svg'
-    this.title = this.title.replace('Fechar', 'Abrir')
-  }
-
-  url.pathname = filePath.join('/')
-  icon.src = url.toString()
-
-  sidebar.classList.toggle('active')
-}
-
-function addFile(file) {
-  const fileList = document.getElementById('file-list')
-  const fileListItemTemplate = document.getElementById('file-list-item-template')
-    .content.querySelector('.file-list-item')
-
+function loadFileOnList(fileName, fileID) {
   const fileListItem = document.importNode(fileListItemTemplate, true)
-  const fileLink = fileListItem.firstElementChild
 
-  fileLink.textContent = file
-  fileLink.title = file
-
-  if (fileList.querySelector('.empty-message')) {
-    fileList.removeChild(fileList.querySelector('.empty-message'))
-  }
+  fileListItem.setAttribute('data-file-id', fileID)
+  fileListItem.firstElementChild.value = fileID
+  fileListItem.firstElementChild.textContent = fileName
 
   fileList.appendChild(fileListItem)
 }
 
-menuButton.addEventListener('click', function() { toggleSidebarVisibility.call(this) })
+function createFile(file) {
+  const fileID = localStorage.getItem('nextFileID')
+
+  localStorage.setItem(fileID, JSON.stringify({
+    fileName: file,
+    content: ''
+  }))
+
+  localStorage.setItem('nextFileID', String(parseInt(fileID) + 1).padStart(6, '0'))
+
+  loadFileOnList(file, fileID)
+}
+
+sidebarButton.addEventListener('click', () => {
+  const sidebar = document.getElementById('sidebar')
+  sidebar.classList.toggle('active')
+})
 
 newFileButton.addEventListener('click', () => fileNameDialog.showModal())
 
@@ -74,8 +64,25 @@ submitButton.addEventListener('click', (event) => {
 })
 
 fileNameDialog.addEventListener('close', function() {
+  submitButton.setAttribute('disabled', '')
+
   if (this.returnValue) {
-    addFile(this.returnValue)
+    createFile(this.returnValue)
     this.returnValue = ''
   }
 })
+
+if (localStorage.getItem('nextFileID') === null) {
+  localStorage.setItem('nextFileID', '000000')
+}
+
+if (localStorage.length > 1) {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+
+    if (!/\d{6}/.test(key)) continue
+
+    const fileName = JSON.parse(localStorage.getItem(key)).fileName
+    loadFileOnList(fileName, key)
+  }
+}
